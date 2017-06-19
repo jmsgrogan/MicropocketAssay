5a9a70f63d5ae141239166fddbb51495763c5908
jmsgrogan <grogan@maths.ox.ac.uk>
2017-06-19 17:17:27 +0100

    Small mods to fitting code.

--- test/python/postprocessing_runner.py
+++ test/python/postprocessing_runner.py
@@ -25,14 +25,17 @@ def make_figure(work_dir, locations, values, eachParam, x_title=r"Position - $\m
         if eachParam.limits[1] is not None:
             ax.set_ylim(eachParam.limits[1])
     
-    ax.axvline(100.0, color='C2', linestyle='--', lw=1)
+    ax.axvline(100.0, color='C0', linestyle='--', lw=1)
     ax.axvline(1100.0, color='C3', linestyle='--', lw=1)
     ax.set_xlabel(x_title) 
     ax.set_ylabel(eachParam.title) 
     
     max_result = 0.0
+    colormap = plt.cm.viridis #nipy_spectral, Set1,Paired   
+    colors = [colormap(i) for i in np.linspace(0, 1,len(values[::eachParam.sampling_frequency]))]
+    
     for idx, eachResult in enumerate(values[::eachParam.sampling_frequency]):
-        ax.plot(locations, np.array(eachResult[1]), color=eachParam.line_color, lw=1)
+        ax.plot(locations, np.array(eachResult[1]), color=colors[idx], lw=1)
         local_max = np.max(np.array(eachResult[1]))
         if local_max > max_result:
             max_result = local_max
@@ -40,7 +43,7 @@ def make_figure(work_dir, locations, values, eachParam, x_title=r"Position - $\m
     ylim = ax.get_ylim()
     ax.set_ylim([0, ylim[1]])
     
-    ax.annotate('Limbus', xy=(150, max_result*0.99), color="C2")
+    ax.annotate('Limbus', xy=(150, max_result*0.99), color="C0")
     ax.annotate('Pellet', xy=(950, max_result*0.99), color="C3")
         
     fig.savefig(work_dir,bbox_inches='tight',dpi=300) 
@@ -62,18 +65,18 @@ def run(work_dir, domain_types, output_params, num_repeats):
 
 if __name__ == '__main__':
 
-    #file_handler = chaste.core.OutputFileHandler("Python/Cornea/ParamSweep/ParamName_sproutingprobability/ParamValue_0", False)
-    file_handler = chaste.core.OutputFileHandler("Python/Cornea/TestSimulationPdeSink", False)
+    file_handler = chaste.core.OutputFileHandler("Python/Cornea/ParamSweep_WithConsumption/ParamName_sproutingprobability/ParamValue_0", False)
+    #file_handler = chaste.core.OutputFileHandler("Python/Cornea/TestSimulationPdeSink", False)
     work_dir = file_handler.GetOutputDirectoryFullPath()
 
     domain_types = ["Planar 2D", "Planar 3D", "Circle 2D", "Circle 3D", "Hemisphere 3D"]
-    #domain_types = ["Planar 2D"]
+    domain_types = ["Planar 2D", "Circle 2D", "Circle 3D"]
     
     output_params = [OutputParameter(name = "Line_density", title = r"Line Density - $\mu m$ per $\mu m^3$",
                                      limits = [[0, 1200], None], line_color = "C0", sampling_frequency=10),
-                     OutputParameter(name = "Tip_density", title = r"Tip Density - $\mu m^-3$",
+                     OutputParameter(name = "Tip_density", title = r"Tip Density - $\mu m^{-3}$",
                                      limits = [[0, 1200], None], line_color = "C0", sampling_frequency=10),
-                     OutputParameter(name = "Branch_density", title = r"Branch Density - $\mu m^-3$",
+                     OutputParameter(name = "Branch_density", title = r"Branch Density - $\mu m^{-3}$",
                                      limits = [[0, 1200], None], line_color = "C0", sampling_frequency=10),
                      OutputParameter(name = "PDE", title = r"Concentration - nanomolar",
                                      limits = [[0, 1200], None], line_color = "C0", sampling_frequency=10),]
