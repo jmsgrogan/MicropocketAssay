ecbabacafc16b82a3c322564f826e131ce1cf6cb
jmsgrogan <grogan@maths.ox.ac.uk>
2017-06-21 15:31:55 +0100

    Replace Python Base simulation with CPP version.

--- src/python/cornea/parameters/default_parameters.py
+++ src/python/cornea/parameters/default_parameters.py
@@ -12,108 +12,111 @@ from cornea.parameters.parameter_collection import Parameter
 _default_collection = cornea.parameters.parameter_collection.SimulationParameterCollection()
 
 # Domain Details
-_default_collection.add_parameter(Parameter("domain type", "Planar 2D"))
-_default_collection.add_parameter(Parameter("cornea radius", 1.3e-3*metre(), 
+_default_collection.add_parameter(Parameter("DomainType", "Planar2D"))
+_default_collection.add_parameter(Parameter("CorneaRadius", 1.3e-3*metre(), 
                                            min_val = 1.0, max_val = 1.0, symbol = "R", 
                                            nice_name = "Cornea Radius"))
-_default_collection.add_parameter(Parameter("cornea thickness", 100.0e-6*metre(), 
+_default_collection.add_parameter(Parameter("CorneaThickness", 100.0e-6*metre(), 
                                            min_val = 1.0, max_val = 1.0, symbol = "T", 
                                            nice_name = "Cornea Thickness"))
-_default_collection.add_parameter(Parameter("pellet height", 1.0e-3*metre(),
+_default_collection.add_parameter(Parameter("PelletHeight", 1.0e-3*metre(),
                                            min_val = 0.8, max_val = 1.2, symbol = "h", 
                                            nice_name = "Pellet Height"))
-_default_collection.add_parameter(Parameter("pellet radius", 200.0e-6*metre(),
+_default_collection.add_parameter(Parameter("PelletRadius", 200.0e-6*metre(),
                                            min_val = 1.0, max_val = 1.0, symbol = "r", 
                                            nice_name = "Pellet Radius")) 
-_default_collection.add_parameter(Parameter("pellet thickness", 40.0e-6*metre(),
+_default_collection.add_parameter(Parameter("PelletThickness", 40.0e-6*metre(),
                                            min_val = 1.0, max_val = 1.0, symbol = "r", 
                                            nice_name = "Pellet Thickness")) 
-_default_collection.add_parameter(Parameter("limbal offset", 100.0e-6*metre(),
+_default_collection.add_parameter(Parameter("LimbalOffset", 100.0e-6*metre(),
                                            min_val = 1.0, max_val = 1.0, symbol = "\epsilon", 
                                            nice_name = "Limbal Offset")) 
 
 # Domain Numerics and Sampling
-_default_collection.add_parameter(Parameter("grid spacing", 40.0e-6*metre(), 
+_default_collection.add_parameter(Parameter("GridSpacing", 40.0e-6*metre(), 
                                            min_val = 1.0, max_val = 1.0, symbol = "\Delta x", 
                                            nice_name = "Grid Spacing")) 
-_default_collection.add_parameter(Parameter("element area 2d", 1e3*(1.e-18*metre_cubed())))
-_default_collection.add_parameter(Parameter("element area 3d", 1e4*(1.e-18*metre_cubed())))                                    
-_default_collection.add_parameter(Parameter("node spacing", 40.0e-6*metre(), 
+_default_collection.add_parameter(Parameter("ElementArea2d", 1e3*(1.e-18*metre_cubed())))
+_default_collection.add_parameter(Parameter("ElementArea3d", 1e4*(1.e-18*metre_cubed())))                                    
+_default_collection.add_parameter(Parameter("NodeSpacing", 40.0e-6*metre(), 
                                            min_val = 1.0, max_val = 1.0, symbol = "\Delta n", 
                                            nice_name = "Node Spacing"))   
-_default_collection.add_parameter(Parameter("density grid spacing", 40.0e-6*metre(),
+_default_collection.add_parameter(Parameter("DensityGridSpacing", 40.0e-6*metre(),
                                            min_val = 1.0, max_val = 1.0, symbol = "\Delta x_{\rho}", 
                                            nice_name = "Density Grid Spacing")) 
-_default_collection.add_parameter(Parameter("sample spacing x", 20.0e-6*metre(),
+_default_collection.add_parameter(Parameter("SampleSpacingX", 60.0e-6*metre(),
                                            min_val = 1.0, max_val = 1.0, symbol = "\Delta x_{s}", 
                                            nice_name = "Sample Spacing X")) 
-_default_collection.add_parameter(Parameter("sample spacing y", 20.0e-6*metre(),
+_default_collection.add_parameter(Parameter("SampleSpacingY", 60.0e-6*metre(),
                                            min_val = 1.0, max_val = 1.0, symbol = "\Delta y_{s}", 
                                            nice_name = "Sample Spacing Y"))   
-_default_collection.add_parameter(Parameter("sample spacing z", 20.0e-6*metre(),
+_default_collection.add_parameter(Parameter("SampleSpacingZ", 33.0e-6*metre(),
                                            min_val = 1.0, max_val = 1.0, symbol = "\Delta z_{s}", 
                                            nice_name = "Sample Spacing Z"))  
 
 # Domain Switches
-_default_collection.add_parameter(Parameter("use pellet", True))  
-_default_collection.add_parameter(Parameter("use finite pellet width", False)) 
+_default_collection.add_parameter(Parameter("UsePellet", True))  
+_default_collection.add_parameter(Parameter("FinitePelletWidth", False)) 
 
 # Angiogenesis details
-_default_collection.add_parameter(Parameter("sprouting probability", 0.5 /(3600.0*second()), 
+_default_collection.add_parameter(Parameter("SproutingProbability", 0.5 /(3600.0*second()), 
                                            min_val = 0.0, max_val = 10.0, symbol = "P_{max}", 
                                            nice_name = "Max Sprouting Probability")) 
-_default_collection.add_parameter(Parameter("attraction strength", 0.0, 
+_default_collection.add_parameter(Parameter("AttractionStrength", 0.0, 
                                            min_val = 0.0, max_val = 10.0, symbol = "S_{att}", 
                                            nice_name = "Attraction Strength")) 
-_default_collection.add_parameter(Parameter("chemotactic strength", 0.5, 
+_default_collection.add_parameter(Parameter("ChemotacticStrength", 0.5, 
                                            min_val = 0.0, max_val = 20.0, symbol = "\chi", 
                                            nice_name = "Chemotactic Strength")) 
-_default_collection.add_parameter(Parameter("persistence angle", 5.0, 
+_default_collection.add_parameter(Parameter("PersistenceAngle", 5.0, 
                                            min_val = 0.0, max_val = 4.0, symbol = "\alpha", 
                                            nice_name = "Persistence Angle")) 
-_default_collection.add_parameter(Parameter("tip exclusion radius", 40.0e-6*metre(), 
+_default_collection.add_parameter(Parameter("SproutVelocity", 20.0 *(1.e-6/3600.0) * metre_per_second(), 
+                                           min_val = 0.1, max_val = 4.0, symbol = "v", 
+                                           nice_name = "Persistence Angle")) 
+_default_collection.add_parameter(Parameter("TipExclusionRadius", 40.0e-6*metre(), 
                                            min_val = 0.0, max_val = 2.0, symbol = "r_{excl}", 
                                            nice_name = "Tip Exclusion Radius")) 
 
 # Angiogenesis switches
-_default_collection.add_parameter(Parameter("do anastomosis", True)) 
+_default_collection.add_parameter(Parameter("DoAnastamosis", True)) 
 
 # PDE Details
-_default_collection.add_parameter(Parameter("pellet concentration", 0.3*mole_per_metre_cubed(), 
+_default_collection.add_parameter(Parameter("PelletConcentration", 0.3*mole_per_metre_cubed(), 
                                            min_val = 0.0, max_val = 1.0, symbol = "C_{0}", 
                                            nice_name = "Initial Pellet Concentration")) 
-_default_collection.add_parameter(Parameter("vegf diffusivity", 6.94e-11 * metre_squared_per_second(), 
+_default_collection.add_parameter(Parameter("VegfDiffusivity", 6.94e-11 * metre_squared_per_second(), 
                                            min_val = 0.1, max_val = 10.0, symbol = "D", 
                                            nice_name = "VEGF Diffusivity")) 
-_default_collection.add_parameter(Parameter("vegf decay rate", (-0.8/3600.0) * per_second(), 
+_default_collection.add_parameter(Parameter("VegfDecayRate", (-0.8/3600.0) * per_second(), 
                                            min_val = 0.1, max_val = 10.0, symbol = "\lambda", 
                                            nice_name = "VEGF Decay Rate")) 
-_default_collection.add_parameter(Parameter("pellet binding constant", 100.0, 
+_default_collection.add_parameter(Parameter("VegfBindingConstant", 100.0, 
                                            min_val = 0.01, max_val = 10.0, symbol = "\theta", 
                                            nice_name = "Pellet Binding Constant")) 
-_default_collection.add_parameter(Parameter("vegf blood concentration", 0.0*mole_per_metre_cubed(), 
+_default_collection.add_parameter(Parameter("VegfBloodConcentration", 0.0*mole_per_metre_cubed(), 
                                            min_val = 0.0, max_val = 1.0, symbol = "c_{b}", 
                                            nice_name = "VEGF Blood Concentration")) 
-_default_collection.add_parameter(Parameter("vessel permeability", (3.e-4/3600.0)*metre_per_second(), 
+_default_collection.add_parameter(Parameter("VesselPermeability", (3.e-4/3600.0)*metre_per_second(), 
                                            min_val = 0.0, max_val = 1.0, symbol = "\rho_{p}", 
                                            nice_name = "Vessel Permeability")) 
-_default_collection.add_parameter(Parameter("uptake rate per cell", (5.e8/3600.0)*mole_per_second(), 
+_default_collection.add_parameter(Parameter("UptakeRatePerCell", (4.e-18/3600.0)*mole_per_second(), 
                                            min_val = 0.0, max_val = 10.0, symbol = "k_{ec}", 
                                            nice_name = "Uptake Rate Per Cell")) 
 
 # PDE Numerics
-_default_collection.add_parameter(Parameter("pde time increment", 0.01)) 
+_default_collection.add_parameter(Parameter("PdeTimeIncrement", 0.01)) 
 
 # PDE Switches
-_default_collection.add_parameter(Parameter("include vessel sink", True))
-_default_collection.add_parameter(Parameter("use fixed gradient", False))
-_default_collection.add_parameter(Parameter("use pde only", False))
+_default_collection.add_parameter(Parameter("IncludeVesselSink", True))
+_default_collection.add_parameter(Parameter("UseFixedGradient", False))
+_default_collection.add_parameter(Parameter("UsePdeOnly", False))
 
 # Simulation Details
-_default_collection.add_parameter(Parameter("total time", 24)) 
-_default_collection.add_parameter(Parameter("time step", 0.5)) 
-_default_collection.add_parameter(Parameter("run number", 0)) 
-_default_collection.add_parameter(Parameter("random seed", 0)) 
+_default_collection.add_parameter(Parameter("TotalTime", 24)) 
+_default_collection.add_parameter(Parameter("TimeStepSize", 0.5)) 
+_default_collection.add_parameter(Parameter("RunNumber", 0)) 
+_default_collection.add_parameter(Parameter("RandomSeed", 0)) 
 
 def get_default_collection():
     return deepcopy(_default_collection)
--- src/python/cornea/simulations/simulation_fixed_gradient.py
+++ src/python/cornea/simulations/simulation_fixed_gradient.py
@@ -1,26 +1,37 @@
 import chaste # Core Chaste functionality
 chaste.init() # Initialize MPI and PETSc
 
-import cornea.simulations.base_simulation
+import microvessel_chaste.simulation
 import cornea.parameters.default_parameters
         
 if __name__ == '__main__':
     
     work_dir = "Python/Cornea/TestSimulationFixedGradient/"
     parameter_collection = cornea.parameters.default_parameters.get_default_collection()
-    parameter_collection.get_parameter("use fixed gradient").value = True
+    parameter_collection.get_parameter("UseFixedGradient").value = True
     
-    domain_types = ["Planar 2D", "Circle 2D", "Planar 3D", "Circle 3D", "Hemisphere 3D"]
+    domain_types = ["Planar_2D", "Circle_2D", "Planar_3D", "Circle_3D", "Hemisphere_3D"]
     #domain_types = ["Planar 2D"]
     random_seeds = [1234]
 
     for eachDomainType in domain_types:
         run_number = 0
-        parameter_collection.get_parameter("domain type").value = eachDomainType
+        parameter_collection.get_parameter("DomainType").value = eachDomainType
         for eachSeed in random_seeds:
-            parameter_collection.get_parameter("run number").value = run_number
-            parameter_collection.get_parameter("random seed").value = eachSeed
+            parameter_collection.get_parameter("RunNumber").value = run_number
+            parameter_collection.get_parameter("RandomSeed").value = eachSeed
             local_work_dir = work_dir + "/DomainType_" + eachDomainType.replace(" ", "") + "/Run_" + str(run_number)
-            simulation = cornea.simulations.base_simulation.BaseSimulation(parameter_collection, local_work_dir)
-            simulation.run()
+            
+            if "2" in eachDomainType:
+                simulation = microvessel_chaste.simulation.CornealMicropocketSimulation2()
+            else 
+                simulation = microvessel_chaste.simulation.CornealMicropocketSimulation3()
+            simulation.SetWorkDir(local_work_dir)
+            for eachParameter in parameter_collection.keys():
+                if eachParameter not in ["DomaintType"]:
+                    getattr(simulation, 'Set'+parameter_collection[eachParameter].name)(parameter_collection[eachParameter].value)
+                    
+            simulation.SetDomainType(getattr(microvessel_chaste.simulation, "DomainType." + eachDomainType))
+
+            simulation.Run()
             run_number += 1
