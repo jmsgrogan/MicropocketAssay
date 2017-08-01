089868b0c3eef396eb4c532740d36e19bfd1350e
jmsgrogan <grogan@maths.ox.ac.uk>
2017-08-01 15:08:20 +0100

    add simple para test.

--- test/python/parallel_launch.py
+++ test/python/parallel_launch.py
@@ -10,15 +10,9 @@ import os
 import time
 import sys
 import pickle
-import chaste
-import microvessel_chaste.simulation
-from microvessel_chaste.utility import *
-#from cornea.simulations.pde_vary_h import study, master_work_dir, study_data
-from cornea.simulations.fg_vary_cp_sprout import study, master_work_dir, study_data
-
 from mpi4py import MPI
 
-n_workers = 3
+n_workers = 16
 start_worker = 'worker'
 usage = 'Program should be started without argument'
 
@@ -28,16 +22,11 @@ if len(sys.argv) == 1:
     # Start clock
     start = MPI.Wtime()
 
-    # Set up the problem, avoid initializing Chaste
-    test_output = os.getenv('CHASTE_TEST_OUTPUT', os.getcwd())
-    if not os.path.exists(test_output + "/" + master_work_dir):
-        os.makedirs(test_output + "/" + master_work_dir)
-    pickle.dump(study_data, open(test_output + "/" + master_work_dir + "/study_data.p", "wb"))
-
     # Don't spawn until this is done
     time.sleep(2)
 
     # Append stop sentinel for each worker
+    study = range(100)
     msg_list = study + ([StopIteration] * n_workers)
 
     # Spawn workers
@@ -78,38 +67,13 @@ elif sys.argv[1] == start_worker:
 
     # Ask for work until stop sentinel
     local_comm = MPI.COMM_WORLD.Split(color=rank, key=rank)
-    chaste.init(comm=local_comm)
+    print "Worker rank", local_comm.Get_rank()
 
     log = []
     for task in iter(lambda: comm.sendrecv(dest=0), StopIteration):
-        study, run_number, domain, pc, work_dir = task
+        print "Worker task", task
+        time.sleep(2)
 
-        file_handler = chaste.core.OutputFileHandler(work_dir, False)
-        int_work_dir = work_dir + "/" + study["name"]
-        domain_string = "/DomainType_" + domain.replace(" ", "")
-        run_string = "/Run_" + str(run_number) + "/"
-        local_ext = domain_string + run_string
-        local_work_dir = int_work_dir + local_ext
-        if "2" in domain:
-            simulation = microvessel_chaste.simulation.CornealMicropocketSimulation2()
-        else:
-            simulation = microvessel_chaste.simulation.CornealMicropocketSimulation3()
-        simulation.SetWorkDir(local_work_dir)
-        for eachParameter in pc.collection.keys():
-            if eachParameter not in ["DomainType", "RandomSeed", "RunNumber"]:
-                param_nam = pc.collection[eachParameter].name
-                param_value = pc.collection[eachParameter].value
-                getattr(simulation, 'Set'+param_nam)(param_value)
-        domain_type = microvessel_chaste.simulation.DomainType
-        domain_key = domain
-        if "Finite" in domain:
-            domain_key = domain[0:-7]
-        simulation.SetRandomSeed(int(pc.get_parameter("RandomSeed").value))
-        simulation.SetRunNumber(int(pc.get_parameter("RunNumber").value))
-        simulation.SetDomainType(getattr(domain_type, domain_key.upper()))
-        simulation.Run()
-        pc.save(file_handler.GetOutputDirectoryFullPath() + study["name"] + "/" +
-                local_ext + "/input_parameters.p")
         log.append(task)
 
     # Collective report to parent
